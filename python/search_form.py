f601f0aae0f598626f1e34bbcd0ade038a3e2809
Susan Tyler <susanltyler@gmail.com>
2016-05-27 15:09:43 +0000

    Deleting unneeded files.

--- python/search_form.py
+++
@@ -1,6 +0,0 @@
-from wtforms import Form, StringField, SubmitField
-from wtforms.validators import Required
-
-class SearchForm(Form):
-    key_word = StringField('Key-word search term') #, validators=[Required()]
-    submit = SubmitField('Submit')
