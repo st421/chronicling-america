150a6e6a0f03bf6cc02538c9f03eb659c838f606
Susan Tyler <susanltyler@gmail.com>
2016-06-08 15:41:45 +0000

    Updated map data to include more properties

--- static/json/map_data/add_state_props.sh
+++ static/json/map_data/add_state_props.sh
@@ -1,7 +1,2 @@
-find *.json | xargs -i topojson -o {} -p name=STATENAM -- {}
-find *.json | xargs -i topojson -o {} -e states.csv --id-property=name -p abbrev=abbreviation -p -- {}
-
-state=&lccn=&dateFilterType=yearRange&date1=1847&date2=1847&language=&ortext=&andtext=&phrasetext=&proxtext=taft&rows=20&searchType=advanced&sort=state&format=json
-&date1=1847&sort=state&rows=20&sequence=0&format=json&date2=1847&dateFilterType=yearRange&page=1&protext=taft
-&date1=1847&sort=state&rows=20&format=json&date2=1847&dateFilterType=yearRange&protext=taft
-&proxtext=taft&date1=1847&date2=1847&dateFilterType=yearRange&rows=20&sort=state&format=json
+# takes TOPOjson from animated-borders-d3js and adds new info about the states
+find *.json | xargs -i topojson -o {} -s 1e-6 -e states.csv --id-property=STATENAM,name -p name=STATENAM,id=+id,abbrev=abbreviation,admit=+admitted -- {}
