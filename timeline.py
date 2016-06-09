d0a691493e195cb7e38cbdf3b85ef67e60d0b49d
Susan Tyler <susanltyler@gmail.com>
2016-06-09 14:45:25 +0000

    Updated territory colors and a couple other miscellaneous fixes

--- timeline.py
+++ timeline.py
@@ -3,14 +3,18 @@ from flask import current_app
 
 class Timeline(object):
 
-    def __init__(self, topic, originalJson):
+    def __init__(self, topic, items):
         current_app.logger.debug("Creating timeline for topic: %s", topic)
         
         self.titleSlide = TimelineSlide(topic)
         self.event_list = []
         
-        for eventJson in originalJson:
-            self.event_list.append(TimelineEvent(eventJson).getSlide())
+        for page in items:
+            try:
+                self.event_list.append(TimelineEvent(page).getSlide())
+            except Exception as e:
+                current_app.logger.info(e)
+                pass
   
     def getEventSlides(self):
         return self.event_list
@@ -36,24 +40,24 @@ class TimelineSlide(object):
     
 class TimelineEvent(TimelineSlide):
 
-    def __init__(self, eventJson):
-        TimelineSlide.__init__(self, self.constructHeadline(eventJson["id"], eventJson["sequence"], eventJson["title"]))
+    def __init__(self, page):
+        TimelineSlide.__init__(self, self.constructHeadline(page["id"], page["sequence"], page["title"]))
         
         self.media_dict = {}
         self.date_dict = {}
         
-        self.addDate(eventJson["date"])
-        self.addMedia(eventJson["id"])
+        self.addDate(page["date"])
+        self.addMedia(page["id"])
         self.event_dict["media"] = self.media_dict
         self.event_dict["start_date"] = self.date_dict
 
-    def addDate(self, originalDate):
-        self.date_dict["year"] = originalDate[0:4]
-        self.date_dict["month"] = originalDate[4:6]
-        self.date_dict["day"] = originalDate[6:8]
+    def addDate(self, date):
+        self.date_dict["year"] = date[0:4]
+        self.date_dict["month"] = date[4:6]
+        self.date_dict["day"] = date[6:8]
 
-    def constructHeadline(self, seqId, pageNum, paperName):
-        return '<a href="{}{}">Page {} from the {}</a>'.format(getBaseUrl(), seqId, pageNum, paperName)
+    def constructHeadline(self, pageId, pageNum, paperName):
+        return "<a href='{}{}'>Page {} from the {}</a>".format(getBaseUrl(), pageId, pageNum, paperName)
         #self.textDict["text"] = item["ocr_eng"]
 
     def addMedia(self, seqId):
--- utils.py
+++ utils.py
@@ -6,6 +6,10 @@ import math
 
 BASE_URL = "http://chroniclingamerica.loc.gov"
 SEARCH_URL = "{}/search/pages/results/?".format(BASE_URL)
+MAX_PAGES = 10
+ITEMS_PER_PAGE = 50
+TIMEOUT = 30
+REQ_THREADS = 5
 
 def getBaseUrl():
     return BASE_URL
@@ -13,7 +17,7 @@ def getBaseUrl():
 def searchParams():
     search_params = {
         "sort":"relevance",
-        "rows":"50",
+        "rows":str(ITEMS_PER_PAGE),
         "sequence":"1",
         "format":"json",
     }
@@ -33,24 +37,23 @@ def mapSearchParams(topic, year):
     return search_params
     
 def exception_handler(request, exception):
-    current_app.logger.debug("Unable to retrieve data from %s: %s", request.path_url, exception)    
+    current_app.logger.error("Unable to retrieve data from %s: %s", request.path_url, exception)    
 
 def retrieveData(params):
     session = Session()
     page_1 = session.get(SEARCH_URL, params=params)
     total_items = page_1.json()["totalItems"]
-    pages = int(math.ceil(total_items/50))
-    if(pages > 100): pages = 100
+    pages = int(math.ceil(total_items/ITEMS_PER_PAGE))
+    if(pages > MAX_PAGES): pages = MAX_PAGES
     reqs = []
     reps = []
     reps.extend(page_1.json()["items"])
     for page_num in range(2,pages):
         #params["page"] = str(page_num)
         #reps.extend(session.get(SEARCH_URL, params=params).json()["items"])
-        reqs.append(grequests.request('GET', SEARCH_URL+"page={}".format(page_num), timeout=30, params=params, session=session))
-    #rs = (grequests.get(u) for u in urls)
-    #def imap(requests, stream=False, size=2, exception_handler=None)
-    for resp in grequests.imap(reqs, False, 5, exception_handler=exception_handler):
+        reqs.append(grequests.request('GET', SEARCH_URL+"page={}".format(page_num), timeout=TIMEOUT, params=params, session=session))
+    for resp in grequests.imap(reqs, False, REQ_THREADS, exception_handler=exception_handler):
         print(resp.request.path_url)
-        reps.extend(resp.json()["items"])
+        js = resp.json()["items"]
+        reps.extend(js)
     return reps
