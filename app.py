b07a34df0bc1d9f6a460ec8bc11c5b40800b6e2e
Susan Tyler <susanltyler@gmail.com>
2016-06-09 15:54:32 +0000

    Changed search form to only take year

--- app.py
+++ app.py
@@ -19,29 +19,27 @@ def getTimelineData(topic):
 	timeline_data["events"] = tl.getEventSlides()
 	return jsonify(timeline_data)
 	
-@app.route('/getMapData/<topic>/<year>')
+@app.route('/getMapData/<year>')
 def getMapData(topic, year):
 	word_counts = makeStateCounts(topic, year)
 	app.logger.debug(word_counts)
 	return jsonify(word_counts)
+	
+def frontPage():
+	return render_template('index.html', form=SearchForm())
 
 @app.route('/', methods=['GET','POST'])
 def main():
 	if request.method == 'POST':
 		form = SearchForm(request.form)
 		if form.validate():
-			key_word = form.key_word.data
 			year = form.year.data
-			if year is 0:
-				return render_template('timeline.html', key_word=key_word)
-			else:
-				return render_template('map.html', key_word=key_word, year=year)
+			return render_template('map.html', year=year)
 		else:
-			flash('Please enter a valid search topic')
-			return render_template('index.html', form=SearchForm())
+			flash('Please choose an election year')
+			return frontPage()
 	else: 
-		form = SearchForm()
-		return render_template('index.html', form=form) 
+		return frontPage()
 
 if __name__ == "__main__":
 	app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)),debug=True)
--- forms.py
+++ forms.py
@@ -1,11 +1,10 @@
 from wtforms import Form, StringField, SelectField, SubmitField, validators
 from wtforms.validators import DataRequired, Length
 
-AVAILABLE_DIG_YEARS = range(1836,1922)
+AVAILABLE_DIG_YEARS = range(1836,1924,4)
 DIG_YEAR_CHOICES = [(i, i) for i in AVAILABLE_DIG_YEARS]
 DIG_YEAR_CHOICES.insert(0, (0, ''))
 
 class SearchForm(Form):
-    key_word = StringField('Key-word search term', [Length(min=3, max=25), DataRequired()], default='')
-    year = SelectField('Year', choices=DIG_YEAR_CHOICES, coerce=int, default=0)
+    year = SelectField('Election year', [DataRequired()], choices=DIG_YEAR_CHOICES, coerce=int, default=0)
     submit = SubmitField('Submit')
