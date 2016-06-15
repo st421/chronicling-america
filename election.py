39a015a6f84a54bc997a9ac657a2fad3657c6551
Susan Tyler <susanltyler@gmail.com>
2016-06-15 08:57:08 +0000

    Map uses pie charts

--- election.py
+++ election.py
@@ -1,5 +1,6 @@
 from flask import current_app
 from math import floor
+from operator import itemgetter
 
 class Election(object):
     red = "red"
@@ -37,12 +38,13 @@ class Election(object):
         
     @staticmethod
     def addData(js):
-        maximum = 0
+        state_maxes = {}
         for candidate in js["candidates"]:
             candidate["color"] = Election.getColor(candidate["party"])
-            current_max = candidate["mentions"]["max"]
-            if current_max > maximum: maximum = current_max
-        js["max"] = maximum
+            print(candidate["mentions"])
+            for state,count in candidate["mentions"].iteritems():
+                state_maxes[state] = state_maxes.get(state,0) + count
+        js["max"] = sorted(state_maxes.values(), reverse=True)[0]
         return js
 	
 class Candidate(object):
